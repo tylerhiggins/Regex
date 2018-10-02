05fa716bfafc46c548335bcc3c31609b2b7262c3
Tyler Higgins <tyler.higgins@wsu.edu>
2018-10-02 11:34:50 -0700

    Created basic functions for classes.
    Created basic 'get' functions as well as finishing the basic constructors for NFA and Transition class

--- src/NFA.java
+++ src/NFA.java
@@ -5,7 +5,25 @@ public class NFA {
   private int final_s;
   private Stack<Transition> t_list;
 
-  public NFA(){
+  public NFA(int s, int f){
+    start = s;
+    final_s = f;
+    t_list = new Stack<Transition>();
+  }
+
+  public void makeTransition() {
+
+  }
+
+  public int getStart(){
+    return start;
+  }
+
+  public int getFinal() {
+    return final_s;
+  }
+
+  public void addTransition() {
 
   }
 }
--- src/Transition.java
+++ src/Transition.java
@@ -3,10 +3,26 @@ import java.util.Stack;
 public class Transition {
   private int state_1;
   private int state_2;
-  private int symbol;
+  private char symbol;
   private Stack<Transition> t_list;
 
-  public Transition(){
+  public Transition(int s1, int s2, char sym){
+    state_1 = s1;
+    state_2 = s2;
+    symbol = sym;
+    t_list = new Stack<Transition>();
 
+  }
+
+  public int getState_1(){
+    return state_1;
+  }
+
+  public int getState_2(){
+    return state_2;
+  }
+
+  public char getSymbol(){
+    return symbol;
   }
 }
