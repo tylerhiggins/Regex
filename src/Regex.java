5e58f3e62f2d1c12399dec1e66d11f4b49d3332c
Tyler Higgins <tyler.higgins@wsu.edu>
2018-10-01 23:13:46 -0700

    Added opening regexpressions.txt
    Added code to enable opening and reading from regexpressions.txt, tested code byreading and printing each individual line.

--- src/Regex.java
+++ src/Regex.java
@@ -1,13 +1,26 @@
-import java.util.Stack;
-import java.util.Scanner;
+import java.io.*;
 
 public class Regex {
 
-
-
-
   public static void main(String[] args){
-    Stack<NFA> nfa_stack = new Stack<NFA>();
-    
+    String file = "regexpressions.txt";
+    String regex = null;
+
+    try {
+      FileReader reader = new FileReader(file);
+      BufferedReader buffRead = new BufferedReader(reader);
+      while((regex = buffRead.readLine()) != null){
+        System.out.println(regex);
+      }
+      reader.close();
+    }
+    catch (FileNotFoundException fex){
+      System.out.println("ERROR: Could not find file '" + file +"'.");
+    }
+    catch (IOException iex) {
+      iex.printStackTrace();
+    }
+    // Stack<NFA> nfa_stack = new Stack<NFA>();
   }
+
 }
