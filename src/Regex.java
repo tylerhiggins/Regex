79bf5acaaf62b9f54a109b7a6522ce94f67d172a
Tyler Higgins <tyler.higgins@wsu.edu>
2018-10-07 23:48:58 -0700

    Testing for valid input.
    Testing the program for valid input, (without using the txt file).

--- src/Regex.java
+++ src/Regex.java
@@ -9,10 +9,8 @@ public class Regex {
     //Stack<String> expressions = readRegex();
     Scanner keyboard = new Scanner(System.in);
     String expressions = keyboard.nextLine();
-    for (int i = 0; i < expressions.length(); i++){
       System.out.println("Transitions for postfix regex " + expressions+":");
       createNFA(expressions);
-    }
   }
 
   public static Stack<String> readRegex(){
@@ -43,30 +41,60 @@ public class Regex {
     Stack<NFA> nfa = new Stack<NFA>();
     NFA fa1;  // Create two nfa instances
     NFA fa2;
+    NFA newNFA;
+    Transition changeState;
+    int newFinal = 0;
     for(int i = 0; i < expression.length(); i++){
       c = expression.charAt(i);
       if(c == '\n'){
         break;
       }
-      else {
-        if (c == '&') {
-          fa2 = nfa.pop();
-          fa1 = nfa.pop();
-          nfa.push();
+      try{
+         if ((c < 'a' || c > 'e') && c != 'E' && c != '&' && c != '|' && c != '*'){
+          throw new Exception("\nCharacter " + c + " is not part of the language.");
         }
-        else if (c == '|'){
-          fa2 = nfa.pop();
-          fa1 = nfa.pop();
-          nfa.push();
-        }
-        else if (c == '*'){
-          fa1 = nfa.pop();
-          nfa.push();
-        }
-        else {
-          nfa.push(new NFA(0,1, Character.toString(c)));
-        }
+        System.out.print(c + ", ");
+        /*else {
+          if (c == '&') {
+            fa2 = nfa.pop();
+            fa1 = nfa.pop();
+            newFinal = fa1.getFinal()+fa2.getFinal();
+            newNFA = new NFA(0, newFinal);
+            for(int j = 0; j < fa1.t_list.size(); j++){
+              newNFA.t_list.push(fa1.t_list.get(j));
+
+            }
+            for (int j = 0; j < fa2.t_list.size(); j++){
+              changeState = fa2.t_list.get(j);
+              changeState.setState_1(changeState.getState_1()+fa1.getFinal());
+              changeState.setState_2(changeState.getState_2()+fa1.getFinal());
+              newNFA.t_list.push(changeState);
+            }
+
+
+          }
+          else if (c == '|'){
+            fa2 = nfa.pop();
+            fa1 = nfa.pop();
+            //nfa.push();
+          }
+          else if (c == '*'){
+            fa1 = nfa.pop();
+            //nfa.push();
+          }
+          else {
+            newNFA = new NFA(0,1);
+            //newNFA.addTransition(0,1,c);
+           //nfa.push(newNFA);
+
+          }
+        }*/
+
+      } catch(Exception ex){
+        System.out.println(ex.getMessage());
+        System.exit(1);
       }
+
     }
   }
 
